54b6fce3eb39529c3aab38d2accce7549982b2e4
TheUmbralPyre <60110016+TheUmbralPyre@users.noreply.github.com>
2022-04-25 19:34:02 +0300

    Add a Project That Containes Data

---
+++ Dealership.Data/Interfaces/ICarsData.cs
@@ -0,0 +1,14 @@
+﻿using Dealership.Data.Models;
+using System.Collections.Generic;
+
+namespace Dealership.Data.Interfaces
+{
+    public interface ICarsData
+    {
+        Car Get(int Id);
+        IEnumerable<Car> GetAll();
+        void Add(Car car);
+        void Update(Car car);
+        void Delete(int id);
+    }
+}
