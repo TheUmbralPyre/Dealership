b45a7ca67815e27e5f717e097eb056fa7d8ed1ea
TheUmbralPyre <60110016+TheUmbralPyre@users.noreply.github.com>
2022-04-29 09:36:10 +0300

    Add Entity Framework Core to the Data Project #2

---
+++ Dealership.Data/Services/DatabaseContext/DealershipDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Dealership.Data.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dealership.Data.Services.DatabaseContext
+{
+    public class DealershipDbContext : DbContext
+    {
+        public DbSet<Car> Cars { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Dealership;Trusted_Connection=True;");
+        }
+    }
+}
---
+++ Dealership.Data/Services/SQLServices/SQLCarsData.cs
@@ -0,0 +1,48 @@
+﻿using Dealership.Data.Interfaces;
+using Dealership.Data.Models;
+using Dealership.Data.Services.DatabaseContext;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Dealership.Data.Services.SQLServices
+{
+    public class SQLCarsData : ICarsData
+    {
+        protected readonly DealershipDbContext db;
+
+        public SQLCarsData(DealershipDbContext db)
+        {
+            this.db = db;
+        }
+
+        public async Task AddAsync(Car car)
+        {
+            db.Add(car);
+            await db.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int Id)
+        {
+            var carToDelete = await db.Cars.FirstOrDefaultAsync(x => x.Id == Id);
+            db.Cars.Remove(carToDelete);
+            await db.SaveChangesAsync();
+        }
+
+        public async Task<Car> GetAsync(int Id)
+        {
+            return await db.Cars.FirstOrDefaultAsync(x => x.Id == Id);
+        }
+
+        public async Task<IEnumerable<Car>> GetAllAsync()
+        {
+            return await db.Cars.ToListAsync();
+        }
+
+        public async Task UpdateAsync(Car carToUpdate)
+        {
+            db.Cars.Update(carToUpdate);
+            await db.SaveChangesAsync();
+        }
+    }
+}
