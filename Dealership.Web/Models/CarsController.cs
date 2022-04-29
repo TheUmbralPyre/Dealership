0cdb72b787bfc1bca1d67ec01786337f2ed89bb1
TheUmbralPyre <60110016+TheUmbralPyre@users.noreply.github.com>
2022-04-29 10:28:47 +0300

    Add Web Project

---
+++ Dealership.Web/Models/CarsController.cs
@@ -0,0 +1,70 @@
+﻿using Dealership.Data.Interfaces;
+using Dealership.Data.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Dealership.Web.Models
+{
+    public class CarsController : Controller
+    {
+        ICarsData db = null;
+
+        public CarsController(ICarsData db)
+        {
+            this.db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = await db.GetAllAsync();
+            return View(model);
+        }
+
+        public async Task<IActionResult> Details(int Id)
+        {
+            var model = await db.GetAsync(Id);
+            return View(model);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int Id)
+        {
+            var model = await db.GetAsync(Id);
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(Car car)
+        {
+            await db.UpdateAsync(car);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Car car)
+        {
+            await db.AddAsync(car);
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int Id)
+        {
+            var model = await db.GetAsync(Id);
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(Car car)
+        {
+            await db.DeleteAsync(car.Id);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Dealership.Web/Startup.cs
@@ -0,0 +1,57 @@
+using Dealership.Data.Interfaces;
+using Dealership.Data.Services.DatabaseContext;
+using Dealership.Data.Services.SQLServices;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Dealership.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+            services.AddScoped<ICarsData, SQLCarsData>();
+            services.AddDbContext<DealershipDbContext>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Cars/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Cars}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
