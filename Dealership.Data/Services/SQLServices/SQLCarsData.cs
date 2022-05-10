7b0174c8236d976e7e9f283eca580c8dbfb014de
TheUmbralPyre <60110016+TheUmbralPyre@users.noreply.github.com>
2022-05-10 16:55:41 +0300

    Update CRUD Operations for CarsData

--- Dealership.Data/Services/SQLServices/SQLCarsData.cs
+++ Dealership.Data/Services/SQLServices/SQLCarsData.cs
@@ -18,25 +18,30 @@ namespace Dealership.Data.Services.SQLServices
 
         public async Task AddAsync(Car car)
         {
+            db.Add(car.Engine);
             db.Add(car);
             await db.SaveChangesAsync();
         }
 
         public async Task DeleteAsync(int Id)
         {
-            var carToDelete = await db.Cars.FirstOrDefaultAsync(x => x.Id == Id);
+            var carToDelete = await db.Cars
+                .Include(e => e.Engine)
+                .FirstOrDefaultAsync(c => c.Id == Id);
             db.Cars.Remove(carToDelete);
             await db.SaveChangesAsync();
         }
 
         public async Task<Car> GetAsync(int Id)
         {
-            return await db.Cars.FirstOrDefaultAsync(x => x.Id == Id);
+            return await db.Cars
+                .Include(e => e.Engine)
+                .FirstOrDefaultAsync(c => c.Id == Id);
         }
 
         public async Task<IEnumerable<Car>> GetAllAsync()
         {
-            return await db.Cars.ToListAsync();
+            return await db.Cars.Include(e => e.Engine).ToListAsync();
         }
 
         public async Task UpdateAsync(Car carToUpdate)
