167138536cd9ecc6bebb826540f9a360b414dfb5
TheUmbralPyre <60110016+TheUmbralPyre@users.noreply.github.com>
2022-05-30 08:08:25 +0300

    Rename Identity Table Names

--- Dealership.Data/Services/DatabaseContext/DealershipDbContext.cs
+++ Dealership.Data/Services/DatabaseContext/DealershipDbContext.cs
@@ -7,6 +7,11 @@ namespace Dealership.Data.Services.DatabaseContext
 {
     public class DealershipDbContext : IdentityDbContext<IdentityUser, IdentityRole, string>
     {
+        /// <summary>
+        /// The Schema Name used for Identity Tables.
+        /// </summary>
+        private const string identitySchemaName = "Identity";
+
         public DbSet<Car> Cars { get; set; }
 
         public DbSet<Engine> Engines { get; set; }
@@ -14,6 +19,47 @@ namespace Dealership.Data.Services.DatabaseContext
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=Dealership;Trusted_Connection=True;");
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Rename Identity Tables so that they Don't have "AspNet" in their Names
+            builder.Entity<IdentityUser>(entity =>
+            {
+                entity.ToTable(name: "User", identitySchemaName);
+            });
+
+            builder.Entity<IdentityRole>(entity =>
+            {
+                entity.ToTable(name: "Role", identitySchemaName);
+            });
+
+            builder.Entity<IdentityUserRole<string>>(entity =>
+            {
+                entity.ToTable(name: "UserRoles", identitySchemaName);
+            });
+
+            builder.Entity<IdentityUserClaim<string>>(entity =>
+            {
+                entity.ToTable(name: "UserClaims", identitySchemaName);
+            });
+
+            builder.Entity<IdentityUserLogin<string>>(entity =>
+            {
+                entity.ToTable(name: "UserLogins", identitySchemaName);
+            });
+
+            builder.Entity<IdentityRoleClaim<string>>(entity =>
+            {
+                entity.ToTable(name: "RoleClaims", identitySchemaName);
+            });
+
+            builder.Entity<IdentityUserToken<string>>(entity =>
+            {
+                entity.ToTable(name: "UserTokens", identitySchemaName);
+            });
         }
     }
 }
