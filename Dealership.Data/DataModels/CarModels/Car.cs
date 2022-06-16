a27618390ca6a60fa4fac26f613bc1d53312f6de
TheUmbralPyre <60110016+TheUmbralPyre@users.noreply.github.com>
2022-06-16 23:14:35 +0300

    Move Car Data Models to Seperate Folder

---
+++ Dealership.Data/DataModels/CarModels/Car.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Dealership.Data.DataModels.CarModels
+{
+    public enum Brand
+    {
+        Volkswagen,
+        Opel,
+        Audi,
+        Mercedes,
+        Smart,
+        BMW,
+        Mini,
+        Porsche,
+        Peugeot,
+        Renault,
+        Citroen,
+        DS,
+        Jaguar,
+        LandRover,
+        Vauxhall,
+        Subaru,
+        Mitsubishi,
+        Nissan,
+        Infiniti,
+        Toyota,
+        Lexus,
+        Suziki,
+        Mazda,
+        Honda,
+        Acura,
+        Ford,
+        Chevrolet,
+        Jeep,
+        GMC,
+        Buick,
+        Dodge,
+        RAM,
+        Cadillac,
+        Lincoln,
+        Chrysler,
+        Tesla,
+        Lucid,
+        Rivian
+    }
+
+    public enum BodyType
+    {
+        Hatchback,
+        Sedan,
+        StationWagon,
+        Coupe,
+        FourDoorCoupe,
+        SUV,
+        Truck,
+        Pickup,
+        Van
+    }
+
+    public enum Transmission
+    {
+        Manual,
+        Automatic,
+        DualClutch,
+        CVT
+    }
+
+    public class Car
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public Brand Brand { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(50)")]
+        [MaxLength(50)]
+        public string ModelName { get; set; }
+
+        [Required]
+        public int Year { get; set; }
+
+        [Required]
+        public BodyType BodyType { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvarchar(50)")]
+        [MaxLength(50)]
+        public string Color { get; set; }
+
+        [Required]
+        public Transmission Transmission { get; set; }
+
+        [Required]
+        public int Mileage { get; set; }
+
+        [Required]
+        public Engine Engine { get; set; }
+
+        public IEnumerable<CarPicture> CarPictures { get; set; }
+
+        public CarPictureThumbnail CarThumbnail { get; set; }
+    }
+}
