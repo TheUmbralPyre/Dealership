6a59bfdfa92e93096e0a23d424f91fa5ebaf8061
TheUmbralPyre <60110016+TheUmbralPyre@users.noreply.github.com>
2022-06-16 23:19:06 +0300

    Add Car Picture Data Models

---
+++ Dealership.Data/DataModels/CarModels/CarPicture.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dealership.Data.DataModels.CarModels
+{
+    public class CarPicture
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string Path { get; set; } 
+
+        public string Original { get; set; }
+
+        public string Slide { get; set; }
+
+    }
+}
---
+++ Dealership.Data/DataModels/CarModels/CarPictureThumbnail.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Dealership.Data.DataModels.CarModels
+{
+    public class CarPictureThumbnail
+    {
+        [Key]
+        public Guid Id { get; set; } = Guid.NewGuid();
+
+        public string Path { get; set; }
+
+    }
+}
